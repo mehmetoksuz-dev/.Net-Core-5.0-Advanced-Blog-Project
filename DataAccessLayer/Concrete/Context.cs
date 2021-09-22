9159d9e278bc4ffbc758c66fb8249a0821569124
Mehmet Öksüz <30608954+mehmetoksuz-dev@users.noreply.github.com>
2021-09-22 23:50:36 +0300

    The structure of the project was organized and added migrations.
    The structure of the project was organized and added migrations.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,25 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext //entity framework'e dahil ettik.
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-SS4L2KH; database=DevBlogDb; integrated security=true;"); //CoreBlogDb
+        }
+        public DbSet<About> Abouts {get; set;}
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        [Key]
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public string BlogImage { get; set; }
+        public DateTime BlogCreateDate { get; set; } //string unutuldu burası bende datetime
+        public bool BlogStatus { get; set; }
+
+        public int CategoryID { get; set; } //rship
+        public Category Category { get; set; } //rship
+
+        public List<Comment> Comments { get; set; } //rship
+    }
+}
